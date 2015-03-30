adcea811a4fcc46967412696c373ae1fb3bfa5e6
kolesnikovakate <kolesnikovakate@gmail.com>
2015-03-30 22:05:08 +0300

    Урок 13, AirDrop

--- README.md
+++
@@ -1,2 +0,0 @@
-# CS193P_Trax
-Developing iOS 8 Apps with Swift, Stanford CS193p
---
+++ Trax/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Trax
+//
+//  Created by Екатерина Колесникова on 27.03.15.
+//  Copyright (c) 2015 kkate. All rights reserved.
+//
+
+import UIKit
+
+struct GPXURL {
+    static let Notification = "GPXURL Radio Station"
+    static let Key = "GPXURL URL Key"
+}
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, handleOpenURL url: NSURL) -> Bool {
+        println("url = \(url)")
+        let center = NSNotificationCenter.defaultCenter()
+        let notification = NSNotification(name: GPXURL.Notification, object: self, userInfo: [GPXURL.Key:url])
+        center.postNotification(notification)
+        return true
+    }
+}
+
