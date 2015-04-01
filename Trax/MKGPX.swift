c13536faac37f587e113eb57d27b45e78f96bcb0
kolesnikovakate <kolesnikovakate@gmail.com>
2015-04-01 15:52:12 +0300

    Урок 14, MapKit

---
+++ Trax/MKGPX.swift
@@ -0,0 +1,32 @@
+//
+//  MKGPX.swift
+//  Trax
+//
+//  Created by Екатерина Колесникова on 01.04.15.
+//  Copyright (c) 2015 kkate. All rights reserved.
+//
+
+import MapKit
+
+extension GPX.Waypoint: MKAnnotation
+{
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+
+    var title: String! { return name }
+
+    var subtitle: String! { return info }
+
+    var thumbnailURL: NSURL? { return getImageURLofType("thumbnail") }
+    var imageURL: NSURL? { return getImageURLofType("large") }
+
+    private func getImageURLofType(type: String) -> NSURL? {
+        for link in links {
+            if link.type == type {
+                return link.url
+            }
+        }
+        return nil
+    }
+}
